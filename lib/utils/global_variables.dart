4c5e17c8c2773128983142e6212c7f4552c785bf
thuyquynh2508 <thuyquynh404@gmail.com>
2023-02-07 00:25:25 +0700

    reusable profile screen UI

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/screens/add_post_screen.dart';
 import 'package:flutter_instagram_clone/screens/feed_screen.dart';
+import 'package:flutter_instagram_clone/screens/profile_screen.dart';
 import 'package:flutter_instagram_clone/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  FeedScreen(),
-  SearchScreen(),
-  AddPostScreen(),
-  Text('notify'),
-  Text('profile'),
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notify'),
+  const ProfileScreen(),
 ];
